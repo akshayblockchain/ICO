e756cf37a953b6a2236a7f31e49fe1efc7b083f8
akshayblockchain <akshayb.blockchain@gmail.com>
2022-07-21 10:50:26 +0530

    Deploy script update

--- scripts/delpoy.js
+++ scripts/delpoy.js
@@ -1,20 +1,7 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 const maxSupply = 10000000;//10M total Token Supply
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy("Akshay","AKKI",maxSupply);
   await token.deployed();
@@ -35,9 +22,6 @@ async function main() {
   await ico.start();
 
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
